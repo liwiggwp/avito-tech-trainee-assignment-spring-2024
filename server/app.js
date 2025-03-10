db297eb43aa67c376c6dd2d2fd72781708a31ca1
Venera Kodo <liwiggwp@gmail.com>
2025-03-10 17:05:03 +0500

    add data movie (taken from api kinpoisk) and get request all movies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
+const { initialMovies } = require("./data");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Получение всех фильмов
+app.get("/moved", (req, res) => {
+  res.json(Object.values(initialMovies));
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
