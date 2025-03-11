887a8f68193af13c17e014c5ad745f9236959d03
Venera Kodo <liwiggwp@gmail.com>
2025-03-11 14:31:26 +0500

    update on server data movie and add api request get movie by id

--- server/app.js
+++ server/app.js
@@ -8,9 +8,21 @@ app.get("/", (req, res) => {
 });
 
 // Получение всех фильмов
-app.get("/moved", (req, res) => {
+app.get("/movie", (req, res) => {
   res.json(Object.values(initialMovies));
 });
+
+// Получение фильма по id
+app.get("/movie/:id", (req, res) => {
+  const movieId = parseInt(req.params.id, 10);
+  const movie = initialMovies.docs.find((movie) => movie.id === movieId);
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send({ message: "Movie not found" });
+  }
+});
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
